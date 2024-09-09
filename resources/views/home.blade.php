73d2c36af5ac11884de530d116b32b76454a6eee
Francescosommellaa <franc.sommella@gmail.com>
2024-09-09 21:21:12 +0200

    doesn't work

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Primi Passi Laravel</title>
+</head>
+
+<body>
+    {{--
+    <h1>{{ messaggio }}</h1>
+
+    <ul>
+        @foreach (nomeitems as nuovonomeitem)
+        <li>{{ nuovonomeitem }}</li>
+        @endforeach
+    </ul>
+
+    @if (count(nuovonomeitem) > 2)
+    <span>Perfetto ci sono più di due elementi nell'array.</span>
+    @else
+    <span>Attenzione: l'array contiene solo due o meno elementi. Assicurati che siano almeno 3</span>
+    @endif --}}
+
+</body>
+
+</html>
